440e5b5a7939534cdacf77c228507416b2a6d095
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-10-03 10:34:18 -0300

    chore: add typeorm

---
+++ ormconfig.js
@@ -0,0 +1,11 @@
+export default {
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: `${
+    process.env.APP_ENV === 'production' ? 'dist' : 'src'
+  }/infra/repositories/postgres/entities/index.{js,ts}`
+}
