d245affd6be7fb427694a0482b5f0a48f5e1e550
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-10-04 10:49:04 -0300

    test: ensure required validators works properly

---
+++ tests/unit/application/validation/validators/required.spec.ts
@@ -0,0 +1,55 @@
+import { RequiredFieldError } from '@/application/errors'
+import {
+  RequiredValidator,
+  StringRequiredValidator
+} from '@/application/validation/validators'
+
+describe('RequiredValidator', () => {
+  it('should return RequiredFieldError if value is null', () => {
+    const sut = new RequiredValidator(null)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('should return RequiredFieldError if value is undefined', () => {
+    const sut = new RequiredValidator(undefined)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredValidator('any_value')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+})
+
+describe('StringRequiredValidator', () => {
+  it('should extends RequiredValidator', () => {
+    const sut = new StringRequiredValidator('any_value')
+
+    expect(sut).toBeInstanceOf(RequiredValidator)
+  })
+
+  it('should return RequiredFieldError if value is empty', () => {
+    const sut = new StringRequiredValidator('')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new StringRequiredValidator('any_value')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+})
