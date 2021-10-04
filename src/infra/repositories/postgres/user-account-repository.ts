ea62eb15f1d9e38c308c8eb4213b29279a989d89
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-10-04 08:03:48 -0300

    feat: implements CheckAccountByEmailRepository

--- src/infra/repositories/postgres/user-account-repository.ts
+++ src/infra/repositories/postgres/user-account-repository.ts
@@ -1,10 +1,13 @@
-import { CheckAccountByUsernameRepository } from '@/domain/contracts/repositories'
+import {
+  CheckAccountByEmailRepository,
+  CheckAccountByUsernameRepository
+} from '@/domain/contracts/repositories'
 import { User } from '@/infra/repositories/postgres/entities'
 import { PostgresRepository } from './repository'
 
 export class UserAccountRepository
   extends PostgresRepository<User>
-  implements CheckAccountByUsernameRepository
+  implements CheckAccountByUsernameRepository, CheckAccountByEmailRepository
 {
   constructor() {
     super(User)
@@ -13,7 +16,14 @@ export class UserAccountRepository
   async checkByUsername(
     input: CheckAccountByUsernameRepository.Input
   ): Promise<boolean> {
-    const user = await this.repository.findOne({ username: input.username })
-    return user !== undefined
+    const account = await this.repository.findOne({ username: input.username })
+    return account !== undefined
+  }
+
+  async checkByEmail(
+    input: CheckAccountByEmailRepository.Input
+  ): Promise<boolean> {
+    const account = await this.repository.findOne({ email: input.email })
+    return account !== undefined
   }
 }
