88ba2ed90c03fc6b33495a2afc413462edd07a92
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-10-05 20:21:03 -0300

    feat: implements HashComparer method

--- src/infra/gateways/bcrypt-cryptography.ts
+++ src/infra/gateways/bcrypt-cryptography.ts
@@ -1,10 +1,14 @@
-import { Hasher } from '@/domain/contracts/gateways'
+import { HashComparer, Hasher } from '@/domain/contracts/gateways'
 import bcrypt from 'bcrypt'
 
-export class BcryptCryptography implements Hasher {
+export class BcryptCryptography implements Hasher, HashComparer {
   constructor(private readonly salt: number) {}
 
   async hash(input: Hasher.Input): Promise<Hasher.Output> {
     return await bcrypt.hash(input.plainText, this.salt)
+  }
+
+  async compare(input: HashComparer.Input): Promise<HashComparer.Output> {
+    return await bcrypt.compare(input.plainText, input.digest)
   }
 }
