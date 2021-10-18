66ba95ba97d0482dd853d0d7099f8d2f5dbb3384
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-10-18 10:16:12 -0300

    feat: add typeorm configs

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,21 @@
-export default {
+module.exports = {
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: `${
-    process.env.APP_ENV === 'production' ? 'dist' : 'src'
-  }/infra/repositories/postgres/entities/index.{js,ts}`
+  entities: [
+    `${
+      process.env.APP_ENV === 'production' ? 'dist' : 'src'
+    }/infra/repositories/postgres/entities/index.{js,ts}`
+  ],
+  migrations: [
+    `${
+      process.env.APP_ENV === 'production' ? 'dist' : 'src'
+    }/infra/migrations/*.{js,ts}`
+  ],
+  cli: {
+    migrationsDir: 'src/infra/migrations'
+  }
 }
